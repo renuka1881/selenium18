13a90da1346c4df5ef6047171900399832dbe8c5
Admin <Admin@SCAL5Event-LT.lan>
2022-07-21 16:21:15 -0700

    hello

---
+++ selenium18/src/test/java/com/javaBasics/Myfirstjava.java
@@ -0,0 +1,14 @@
+package com.javaBasics;
+
+public class Myfirstjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is my very first java programs");
+	}
+
+}
+
+
+
+
