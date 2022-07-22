d33dda2f2c782dc4d897a2909a8680f321b94cb7
Admin <Admin@SCAL5Event-LT.lan>
2022-07-21 18:34:11 -0700

    hello

--- selenium18/src/test/java/com/javaBasics/Myfirstjava.java
+++ selenium18/src/test/java/com/javaBasics/Myfirstjava.java
@@ -4,7 +4,7 @@ public class Myfirstjava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("this is my very first java programs");
+System.out.println("this is my very first java program");
 	}
 
 }
